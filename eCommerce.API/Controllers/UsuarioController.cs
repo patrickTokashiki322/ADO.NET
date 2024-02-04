a1f15d04aea90da5220cfa8dd1797b04805c2ecd
patrickTokashiki322 <patricktokashikilima@outlook.com>
2024-02-04 02:14:58 -0300

    Adicionar arquivos de projeto.

---
+++ eCommerce.API/Controllers/UsuarioController.cs
@@ -0,0 +1,69 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioRepository _repository;
+
+        public UsuarioController()
+        {
+            _repository = new UsuarioRepository();
+        }
+
+        /*
+         * CRUD
+         * - GET - Obter lista de usuários
+         * - GET - Obter o usuário passando o ID
+         * - POST - Cadastrar um usuário
+         * - PUT - Atualizar um usuário
+         * - DELETE - Remover um usuário
+         * **/
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get()); // http 200
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,52 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repository;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario() { Id = 1, Nome = "Patrick", Email = "patricktokashikilima@outlook.com"},
+            new Usuario() { Id = 2, Nome = "Vanessa", Email = "vanessatokashiki@outlook.com"},
+            new Usuario() { Id = 3, Nome = "Maya", Email = "mayatokashiki@outlook.com"},
+        };
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+
+            if (ultimoUsuario == null)
+            {
+                usuario.Id = 1;
+            } else
+            {
+                usuario.Id = ultimoUsuario.Id;
+                usuario.Id++;
+            }
+
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
