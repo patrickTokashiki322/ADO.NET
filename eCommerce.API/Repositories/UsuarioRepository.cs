e433aa04dd45aedf7286e9d4a6551305102e2cb1
Patrick Lima <patricktokashikilima@outlook.com>
2024-02-11 02:00:24 -0300

    Adicionado Endereço de entrega e Departamentos ao GET por ID

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -3,13 +3,6 @@ using eCommerce.API.Repository;
 using System.Data;
 using System.Data.SqlClient;
 
-/*
- * Connection => Estabelecer conexão com banco.
- * Command => Insert, Update, Delete -> Comandos sem retorno ou com poucos retornos
- * DataReader => Arq. Conectada. Se mantém conectado no banco, não guarda informações na memória, impedindo outras consultas de serem executadas na mesma tabela.
- * DataAdapter => Arq. Desconectada. Baixa as informações para a memória e se desconecta do banco, permitindo outras consultas na mesma tabela.
- * **/
-
 namespace eCommerce.API.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
@@ -62,7 +55,19 @@ namespace eCommerce.API.Repositories
             try
             {
                 SqlCommand command = new SqlCommand();
-                command.CommandText = $"SELECT * FROM Usuarios WHERE Id = @id";
+                command.CommandText = @"
+SELECT *, 
+	u.Id UsuarioId, 
+	c.Id ContatoId, 
+	c.UsuarioId ContatoUsuarioId,
+	ee.Id EnderecosEntregaId, 
+    ee.UsuarioId EnderecoUsuarioId
+FROM Usuarios u 
+	LEFT JOIN Contatos c ON c.UsuarioId = u.Id 
+	LEFT JOIN EnderecosEntrega ee ON ee.UsuarioId = u.Id 
+	LEFT JOIN UsuariosDepartamentos ud ON ud.UsuarioId = u.Id
+    LEFT JOIN Departamentos d ON ud.DepartamentoId = d.Id
+WHERE u.Id = @id";
                 command.Parameters.AddWithValue("@id", id);
                 command.Connection = (SqlConnection)_connection;
 
@@ -70,21 +75,71 @@ namespace eCommerce.API.Repositories
 
                 SqlDataReader dataReader = command.ExecuteReader();
 
+                Dictionary<int, Usuario> usuarioPopulado = new Dictionary<int, Usuario>();
+
                 while (dataReader.Read())
                 {
                     Usuario usuario = new Usuario();
-                    usuario.Id = dataReader.GetInt32("Id");
-                    usuario.Nome = dataReader.GetString("Nome");
-                    usuario.Email = dataReader.GetString("Email");
-                    usuario.Sexo = dataReader.GetString("Sexo");
-                    usuario.RG = dataReader.GetString("RG");
-                    usuario.CPF = dataReader.GetString("CPF");
-                    usuario.NomeMae = dataReader.GetString("NomeMae");
-                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
-                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
 
-                    return usuario;
+                    if (!(usuarioPopulado.ContainsKey(dataReader.GetInt32("UsuarioId"))))
+                    {
+                        usuario.Id = dataReader.GetInt32("UsuarioId");
+                        usuario.Nome = dataReader.GetString("Nome");
+                        usuario.Email = dataReader.GetString("Email");
+                        usuario.Sexo = dataReader.GetString("Sexo");
+                        usuario.RG = dataReader.GetString("RG");
+                        usuario.CPF = dataReader.GetString("CPF");
+                        usuario.NomeMae = dataReader.GetString("NomeMae");
+                        usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                        usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                        Contato contato = new Contato();
+                        contato.Id = dataReader.GetInt32("ContatoId");
+                        contato.UsuarioId = dataReader.GetInt32("ContatoUsuarioId");
+                        contato.Telefone = dataReader.GetString("Telefone");
+                        contato.Celular = dataReader.GetString("Celular");
+
+                        usuario.Contato = contato;
+
+                        usuarioPopulado.Add(usuario.Id, usuario);
+                    }
+                    else
+                    {
+                        usuario = usuarioPopulado[dataReader.GetInt32("UsuarioId")];
+                    }
+
+                    EnderecoEntrega enderecoEntrega = new EnderecoEntrega();
+                    enderecoEntrega.Id = dataReader.GetInt32("EnderecosEntregaId");
+                    enderecoEntrega.UsuarioId = dataReader.GetInt32("EnderecoUsuarioId");
+                    enderecoEntrega.NomeEndereco = dataReader.GetString("NomeEndereco");
+                    enderecoEntrega.CEP = dataReader.GetString("CEP");
+                    enderecoEntrega.Estado = dataReader.GetString("Estado");
+                    enderecoEntrega.Cidade = dataReader.GetString("Cidade");
+                    enderecoEntrega.Bairro = dataReader.GetString("Bairro");
+                    enderecoEntrega.Endereco = dataReader.GetString("Endereco");
+                    enderecoEntrega.Numero = dataReader.GetString("Numero");
+                    enderecoEntrega.Complemento = dataReader.GetString("Complemento");
+
+                    usuario.EnderecosEntrega = (usuario.EnderecosEntrega == null) ? new List<EnderecoEntrega>() : usuario.EnderecosEntrega;
+
+                    if (usuario.EnderecosEntrega.FirstOrDefault(a => a.Id == enderecoEntrega.Id) == null)
+                    {
+                        usuario.EnderecosEntrega.Add(enderecoEntrega);
+                    }
+
+                    Departamento departamento = new Departamento();
+                    departamento.Id = dataReader.GetInt32(26);
+                    departamento.Nome = dataReader.GetString(27);
+
+                    usuario.Departamentos = (usuario.Departamentos == null) ? new List<Departamento>() : usuario.Departamentos;
+
+                    if (usuario.Departamentos.FirstOrDefault(a => a.Id == departamento.Id) == null)
+                    {
+                        usuario.Departamentos.Add(departamento);
+                    }
                 }
+
+                return usuarioPopulado[usuarioPopulado.Keys.First()];
             }
             finally
             {
@@ -142,7 +197,7 @@ namespace eCommerce.API.Repositories
                 _connection.Open();
 
                 command.ExecuteNonQuery();
-            } 
+            }
             finally
             {
                 _connection.Close();
